956efb149e3d10528ecc4336905ffa2db4d66b88
toriqulislamrupai <toriqulislamrupai@gmail.com>
2024-07-04 18:35:24 +0600

    added a new version project

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2 className="text-3xl">Login page</h2>
+        </div>
+    );
+};
+
+export default Login;
